5359e520e6a59b3c4bd577fa56ccefd1f8ef31b0
Qmsrry <73188139+Qmsrry@users.noreply.github.com>
2021-06-08 19:56:42 +0800

    添加了true函数

--- IndexManager.py
+++ IndexManager.py
@@ -56,8 +56,45 @@ def new_root(share, is_leaf, fmt, index_offset):
     return page_no
 
 
-def true(item, condition):
-    pass
+def true(record,table_name,condition):#判断record是否满足其中的table符合condition，并输出符合的cols，比如true(record,sname,student,age>18)就是搜索record里面的student表当中符合age>18的，输出sname列。
+    nowtable=None
+    if table_name in record:#如果这个record记录中存在这个table
+        nowtable=record[table_name]
+        #现在进入到record的table_name列，例如{"table1":{"name", "data_num", "index_num","fmt","column_list","index_list","primary_key"}"
+        #table2":{"name", "data_num", "index_num","fmt","column_list","index_list","primary_key"}}为record，table1为table_name，
+        # 那么现在nowtable为{"name", "data_num", "index_num","fmt","column_list","index_list","primary_key"}
+    else:
+        return False
+        #否则，这个record里面不存在这个table，那么就找不到符合条件的值
+    nowtable_columnlist=nowtable["column_list"]
+    if condition is not None:
+        exps=condition.split('and')#将不同条件分隔开
+        if len(exps)==1:#如果只有一个条件
+            exp=exps[0].strip()
+            match=re.match(r'^([A-Za-z0-9_]+)\s*([<>=]+)\s*(.+)$', exp, re.S)
+
+            if match and len(match.groups())==3:
+                nowcol,op,value=match.groups()
+                '''
+                例如age>18,nowcol为"age"，op为">",value为"18"
+                '''
+                value=eval(value)#将"18"转换为18，字符串转换为数组
+                for col in nowtable_columnlist:
+                    if nowcol==col:#如果当前的列是要寻找的列
+                        if(op=="="):
+                            return nowtable_columnlist[nowcol]==value
+                        elif op==">":
+                            return nowtable_columnlist[nowcol]>value
+                        elif op=="<":
+                            return nowtable_columnlist[nowcol]<value
+                        elif op==">=":
+                            return nowtable_columnlist[nowcol]>=value
+                        elif op=="<=":
+                            return nowtable_columnlist[nowcol]<=value
+                        elif op=="!=":
+                            return nowtable_columnlist[nowcol]!=value
+    else:
+        return True  #r如果没有条件，那么返回true
 
 
 def delete_tree(share, table_name,condition=None):
